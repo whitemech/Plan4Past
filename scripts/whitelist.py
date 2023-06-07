dc40ed3c148e72b3b2f8a79e9f012160309c42ac
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2023-06-07 11:32:58 -0400

    fix workflows

--- scripts/whitelist.py
+++ scripts/whitelist.py
@@ -1,3 +1,6 @@
 # flake8: noqa
 # type: ignore
 # pylint: skip-file
+cli # function-redefined (plan4past/__main__.py:84)
+domain_parser # unspecified-encoding (plan4past/__main__.py:110)
+problem_parser # unspecified-encoding (plan4past/__main__.py:111)
