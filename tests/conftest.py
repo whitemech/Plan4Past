82ee6ebc81f15aabe4a8ac936275bc81cbace725
Marco Favorito <marco.favorito@gmail.com>
2023-07-07 15:23:58 +0200

    test: add tests for mapping_parser module

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2021 -- 2023 WhiteMech
+#
+# ------------------------------
+#
+# This file is part of Plan4Past.
+#
+# Plan4Past is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Plan4Past is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Plan4Past.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""This module contains fixtures for the tests."""
+import inspect
+from pathlib import Path
+
+ROOT_DIR = Path(inspect.getfile(inspect.currentframe())).parent.parent  # type: ignore
+EXAMPLES_DIR = ROOT_DIR / "examples"
+
+EXAMPLE_MAP_FILE = EXAMPLES_DIR / "pddl/p-0.map"
