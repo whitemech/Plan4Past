539c6324d8ead725185a809ae3bfcc6c20f116df
LBonassi95 <l.bonassi005@unibs.it>
2023-09-14 19:33:25 +0200

    Working on DNF

---
+++ plan4past/utils/nnf_visitor.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2021 -- 2023 WhiteMech
+#
+# ------------------------------
+#
+# This file is part of Plan4Past.
+#
+# Plan4Past is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Plan4Past is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Plan4Past.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""NNF visitor."""
+from functools import singledispatch
+from plan4past.utils.negate_visitor import negate
+from pddl.logic.base import *
+
+
+@singledispatch
+def nnf(obj: Formula) -> Formula:
+    """Computes the nnf of a formula"""
+    raise ValueError(f"object of type {type(obj)} is not supported by this function")
+
+
+@nnf.register
+def _(formula: And) -> Formula:
+    """Computes the nnf of a conjunction"""
+    return And(*[nnf(operand) for operand in formula.operands])
+
+
+@nnf.register
+def _(formula: Or) -> Formula:
+    """Computes the nnf of a disjunction"""
+    return Or(*[nnf(operand) for operand in formula.operands])
+
+@nnf.register
+def _(formula: Atomic) -> Formula:
+    """Computes the nnf of an atom"""
+    return formula
+
+@nnf.register
+def _(formula: Not) -> Formula:
+    """Computes the nnf of a negation"""
+    return nnf(negate(formula.argument))
--- plan4past/utils/ppnf_visitor.py
+++ plan4past/utils/ppnf_visitor.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2021 -- 2023 WhiteMech
+#
+# ------------------------------
+#
+# This file is part of Plan4Past.
+#
+# Plan4Past is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Plan4Past is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Plan4Past.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""Pnf visitor."""
+
 from plan4past.helpers.quoted_atom_helper import get_before_atom
 from functools import singledispatch
 from pylogics.syntax.pltl import *
