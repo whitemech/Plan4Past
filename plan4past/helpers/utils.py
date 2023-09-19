1551e9c08c4f62808f615596977fb294fd69b7f4
LBonassi95 <l.bonassi005@unibs.it>
2023-09-19 16:53:35 +0200

    Working implementation with DNF goal encoding

--- plan4past/helpers/utils.py
+++ plan4past/helpers/utils.py
@@ -26,9 +26,7 @@ from typing import Dict
 from pddl.logic import Predicate, constants
 from pylogics.syntax.base import Formula
 from pylogics.syntax.pltl import Atomic as PLTLAtomic
-
 from plan4past.utils.atoms_visitor import find_atoms
-
 
 def add_val_prefix(name: str):
     """Add the 'prime' prefix."""
